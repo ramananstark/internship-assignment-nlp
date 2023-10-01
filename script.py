ea2956241f5c51c9b44d4d7ab1906a27b3e872cf
ramananstark <ramanan9600@gmail.com>
2023-10-01 16:25:43 +0530

    modified:internship_commit_ramanan   script.py 	modified:internship_commit_ramanan   Report.txt 	modified:internship_commit_ramanan   script.py

--- script.py
+++ script.py
@@ -1,8 +1,28 @@
-## Import all the dependent libraries
-import os
+# ## Import all the dependent libraries
+# import os
+# import tensorflow as tf
+# import 
+
+import tensorflow
+from tensorflow.keras.preprocessing.text import Tokenizer
 
 
-def get_mca_questions(context: str):
-   mca_questions = [“mca1”,”mca2”,”mca3”....]
+# def get_mca_questions(context: str):
+#    mca_questions = ["mca1","mca2","mca3"]p
    
-   return mca_questions  
+#    return mca_questions  
+
+import fitz
+
+pdffile=fitz.open("C:\\Users\\Sivaprasath\\Desktop\\RAMANAN\\akaike\\internship-assignment-nlp\\Dataset\\chapter-2.pdf")
+
+extracted=''
+
+for page_num in range(pdffile.page_count):
+    page=pdffile.load_page(page_num)
+    text=page.get_text()
+    extracted+=text
+
+print(extracted)
+
+tokenizer=Tokenizer(num_words=10000,oov_token=<OOV>)
